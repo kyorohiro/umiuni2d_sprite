ce6a47baf738b5ed708d43986d7a76a9219156dc
kyorohiro <kyorohiro@gmail.com>
2018-02-19 00:51:18 +0900

    add utils

---
+++ lib/umiuni2d_sprite_utils.dart
@@ -0,0 +1,16 @@
+library tinygame_ex;
+
+
+import 'dart:async';
+import 'dart:math'as math;
+import 'dart:convert' as conv;
+import 'package:vector_math/vector_math_64.dart';
+//import 'dart:typed_data';
+import 'umiuni2d_sprite.dart';
+
+
+//part 'tinygame_ex/controller/button.dart';
+//part 'tinygame_ex/controller/joystick.dart';
+part 'utils/texture_atlas/spritesheet.dart';
+part 'utils/texture_atlas/spritesheetinfo.dart';
+part 'utils/texture_atlas/bitmapfont.dart';
---
+++ lib/utils/texture_atlas/spritesheet.dart
@@ -0,0 +1,137 @@
+part of tinygame_ex;
+
+abstract class SpriteSheet {
+  Iterable<Object> get keys;
+  int get length;
+  SpriteSheetData operator [](String key);
+  bool containsKey(String key);
+  SpriteSheet() {}
+  factory SpriteSheet.spritsheet(String json) {
+    return new SpriteSheetInfo.fronmJson(json);
+  }
+
+  factory SpriteSheet.bitmapfont(String json, int w, int h) {
+    return new BitmapFont(json, w, h);
+  }
+
+  Paint p = new Paint();
+  void drawImage(Stage stage, Canvas canvas, Image image, String name) {
+    if (this[name] != null) {
+      canvas.drawImageRect(stage, image, this[name].srcRect, this[name].dstRect, p);
+    }
+  }
+
+  void drawText(Stage stage, Canvas canvas, Image image, String text, double size, {Rect rect: null, BitmapFontInfoType orientation: BitmapFontInfoType.horizontal, double margine: 5.0}) {
+    if(orientation == BitmapFontInfoType.horizontal) {
+      drawTextHorizontal(stage, canvas, image, text, size, rect: rect, margine: margine);
+    } else {
+      drawTextVertical(stage, canvas, image, text, size, rect: rect, margine: margine);
+    }
+  }
+
+  void drawTextHorizontal(Stage stage, Canvas canvas, Image image, String text, double size, {Rect rect: null, double margine: 5.0}) {
+    if(rect == null) {
+      rect = new Rect(0.0,0.0,10000.0,10000.0);
+    }
+    double x = rect.x;
+    double y = rect.y;
+    for (int i = 0; i < text.length; i++) {
+      SpriteSheetData d = this[text[i]];
+      if (d == null) {
+        continue;
+      }
+      Rect dstRect = d.dstRect;
+      dstRect.x = x;
+      dstRect.y = y;
+      dstRect.w = size * d.srcRect.w / d.srcRect.h;
+      dstRect.h = size;
+      if (rect != null) {
+        if((rect.x+rect.w) < (dstRect.x + dstRect.w)){
+          x = rect.x;
+          y += dstRect.h + margine;
+          dstRect.x = x;
+          dstRect.y = y;
+        }
+      }
+      canvas.drawImageRect(stage, image, d.srcRect, dstRect, p);
+      x += dstRect.w + margine * d.srcRect.w / d.srcRect.h;
+    }
+  }
+
+  //
+  void drawTextVertical(Stage stage, Canvas canvas, Image image, String text, double size,
+      {Rect rect: null, double margine: 5.0}) {
+    if(rect == null) {
+      rect = new Rect(0.0,0.0,10000.0,10000.0);
+    }
+    double x = 0.0;
+    double y = 0.0;
+
+    x = rect.x+rect.w;
+    y = rect.y;
+    double s = 0.0;
+    for (int i = 0; i < text.length; i++) {
+      SpriteSheetData d = this[text[i]];
+      if (d == null) {
+        continue;
+      }
+      Rect dstRect = d.dstRect;
+      if(s <(size * d.srcRect.w / d.srcRect.h)) {
+        s = size * d.srcRect.w / d.srcRect.h;
+      }
+      dstRect.w = size * d.srcRect.w / d.srcRect.h;
+      dstRect.h = size;
+      dstRect.x = x-dstRect.w;
+      dstRect.y = y;
+
+      if (rect != null) {
+        if((rect.y+rect.h) < (dstRect.y + dstRect.h)){
+          y = rect.y;
+          x -= s;
+          s = 0.0;
+          dstRect.x = x-dstRect.w;
+          dstRect.y = y;
+        }
+      }
+      canvas.drawImageRect(stage, image, d.srcRect, dstRect, p);
+      y += dstRect.h + margine;
+    }
+  }
+}
+
+abstract class SpriteSheetData {
+  String name;
+  Rect get dstRect;
+  Rect get srcRect;
+  double get angle;
+}
+
+class BitmapFont extends SpriteSheet {
+  Map<Object, BitmapFontData> d = {};
+  int imageWidth;
+  int imageHeight;
+  BitmapFont(String json, this.imageWidth, this.imageHeight) {
+    BitmapFontInfo info = new BitmapFontInfo.fromJson(json);
+    for (int key in info.r.keys) {
+      String name = new String.fromCharCode(key);
+      BitmapFontData v = new BitmapFontData(name, info.r[key], imageWidth.toDouble(), imageHeight.toDouble());
+      d[key] = v;
+      d[name] = v;
+    }
+  }
+  Iterable<Object> get keys => d.keys;
+  int get length => d.length;
+  SpriteSheetData operator [](String key) => d[key];
+  bool containsKey(String key) => d.containsKey(key);
+}
+
+class BitmapFontData extends SpriteSheetData {
+  BitmapFontData(this.name, this.elm, this.imageWidth, this.imageHeight) {}
+  String name;
+  BitmapFontInfoElem elm;
+  double imageWidth;
+  double imageHeight;
+  Rect get dstRect => elm.dstRect(imageWidth, imageHeight);
+  Rect get srcRect => elm.srcRect(imageWidth, imageHeight);
+  double get angle => 0.0;
+}
