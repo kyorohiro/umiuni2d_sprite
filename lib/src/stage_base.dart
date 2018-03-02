09df238f8a323ceeedbc8de09dbfcdcf038aeb3a
kyorohiro <kyorohiro@gmail.com>
2018-03-02 11:05:52 +0900

    ..

--- lib/src/stage_base.dart
+++ lib/src/stage_base.dart
@@ -74,14 +74,14 @@ class StageBase {
   Matrix4 getMatrix() {
     return mats.last;
   }
-
+/*
   double get xFromMat => this.mats.last.storage[12];
   double get yFromMat => this.mats.last.storage[13];
   double get zFromMat => this.mats.last.storage[14];
   double get sxFromMat => (new Vector3(mats.last.storage[0], mats.last.storage[4], mats.last.storage[8])).length;
   double get syFromMat => (new Vector3(mats.last.storage[1], mats.last.storage[5], mats.last.storage[9])).length;
   double get szFromMat => (new Vector3(mats.last.storage[2], mats.last.storage[6], mats.last.storage[10])).length;
-
+*/
   /**
    * call in onTouch
    * For x, y demention
